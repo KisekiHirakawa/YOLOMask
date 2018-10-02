393b7831e6c010c573e723fb4851e93fa4a57ab4
Kiseki Hirakawa <selkistak100@gmail.com>
2018-10-02 06:08:03 +0100

    Changes to Object_detection.py

--- cvlib/object_detection.py
+++ cvlib/object_detection.py
@@ -10,7 +10,7 @@ classes = None
 COLORS = np.random.uniform(0, 255, size=(80, 3))
 
 def populate_class_labels():
-
+    
     class_file_name = 'yolov3_classes.txt'
     class_file_abs_path = dest_dir + os.path.sep + class_file_name
     url = 'https://github.com/arunponnusamy/object-detection-opencv/raw/master/yolov3.txt'
@@ -19,7 +19,7 @@ def populate_class_labels():
     f = open(class_file_abs_path, 'r')
     classes = [line.strip() for line in f.readlines()]
 
-    return classes
+return classes
 
 
 def get_output_layers(net):
@@ -27,84 +27,84 @@ def get_output_layers(net):
     layer_names = net.getLayerNames()
     
     output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
+    
     return output_layers
 
 
 def draw_bbox(img, bbox, labels, confidence, colors=None, write_conf=False):
-
+    
     global COLORS
     global classes
-
+    
     if classes is None:
         classes = populate_class_labels()
-    
-    for i, label in enumerate(labels):
 
+    for i, label in enumerate(labels):
+        
         if colors is None:
-            color = COLORS[classes.index(label)]            
+            color = COLORS[classes.index(label)]
         else:
             color = colors[classes.index(label)]
-
+        
         if write_conf:
             label += ' ' + str(format(confidence[i] * 100, '.2f')) + '%'
 
-        cv2.rectangle(img, (bbox[i][0],bbox[i][1]), (bbox[i][2],bbox[i][3]), color,-1)
-        cv2.putText(img, label, (bbox[i][0],bbox[i][1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        cv2.rectangle(img, (bbox[i][0],bbox[i][1]), (bbox[i][2],bbox[i][3]), color, -1)
 
+cv2.putText(img, label, (bbox[i][0],bbox[i][1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-    return img
-    
+return img
+
 def detect_common_objects(image):
-
+    
     Height, Width = image.shape[:2]
     scale = 0.00392
-
+    
     global classes
     global dest_dir
-
+    
     config_file_name = 'yolov3.cfg'
     config_file_abs_path = dest_dir + os.path.sep + config_file_name
-
+    
     weights_file_name = 'yolov3.weights'
-    weights_file_abs_path = dest_dir + os.path.sep + weights_file_name    
+    weights_file_abs_path = dest_dir + os.path.sep + weights_file_name
     
     url = 'https://github.com/arunponnusamy/object-detection-opencv/raw/master/yolov3.cfg'
-
+    
     if not os.path.exists(config_file_abs_path):
         download_file(url=url, file_name=config_file_name, dest_dir=dest_dir)
-
+    
     url = 'https://pjreddie.com/media/files/yolov3.weights'
 
-    if not os.path.exists(weights_file_abs_path):
-        download_file(url=url, file_name=weights_file_name, dest_dir=dest_dir)    
-
+if not os.path.exists(weights_file_abs_path):
+    download_file(url=url, file_name=weights_file_name, dest_dir=dest_dir)
+    
     global initialize
     global net
-
+    
     if initialize:
         classes = populate_class_labels()
         net = cv2.dnn.readNet(weights_file_abs_path, config_file_abs_path)
         initialize = False
 
-    blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
 
-    net.setInput(blob)
+net.setInput(blob)
 
-    outs = net.forward(get_output_layers(net))
+outs = net.forward(get_output_layers(net))
 
-    class_ids = []
-    confidences = []
-    boxes = []
-    conf_threshold = 0.5
+class_ids = []
+confidences = []
+boxes = []
+conf_threshold = 0.5
     nms_threshold = 0.4
-
+    
     for out in outs:
         for detection in out:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > 0.5 and class_id=='person':
+            if confidence > 0.01:
                 center_x = int(detection[0] * Width)
                 center_y = int(detection[1] * Height)
                 w = int(detection[2] * Width)
@@ -116,22 +116,23 @@ def detect_common_objects(image):
                 boxes.append([x, y, w, h])
 
 
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
 
-    bbox = []
-    label = []
-    conf = []
+bbox = []
+label = []
+conf = []
 
-    for i in indices:
-        i = i[0]
-        box = boxes[i]
-        x = box[0]
-        y = box[1]
-        w = box[2]
-        h = box[3]
-        if str(classes[class_ids[i]])=='person':
-            bbox.append([round(x), round(y), round(x+w), round(y+h)])
-            label.append(str(classes[class_ids[i]]))
-            conf.append(confidences[i])
+for i in indices:
+    i = i[0]
+    box = boxes[i]
+    x = box[0]
+    y = box[1]
+    w = box[2]
+    h = box[3]
+    if str(classes[class_ids[i]])=='person':
+        bbox.append([round(x), round(y), round(x+w), round(y+h)])
+        label.append(str(classes[class_ids[i]]))
+        conf.append(confidences[i])
     
     return bbox, label, conf
+
